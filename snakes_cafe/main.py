c542f3a1ec484bd33ea501b1f9f5e0e811367221
Yonatan Palagashvili <yonimotek@gmail.com>
2020-11-09 11:45:57 -0800

    starting

---
+++ snakes_cafe/main.py
@@ -0,0 +1,7 @@
+intro = "**************************************\n **    Welcome to the Snakes Cafe!   **\n **    Please see our menu below.    **\n **\n ** To quit at any time, type \"quit\" **\n **************************************"
+print(f"{intro}")
+appetizers = ["Wings", "Spring Rolls"]
+entrees = ["Salmon","Steak","Meat Tornado", "A Literal Garden"]
+desserts=["Ice Cream", "Cake", "Pie"]
+drinks=["Coffee","Tea","Unicorn Tears"]
+menu=[f"{appetizers}", f"{entrees}", f"{desserts}", f"{drinks}"]
